70913eb02dfa2785428f06720f847f58f9522616
TychoBrouwer <tychobrouwer33@gmail.com>
2020-04-11 23:48:25 +0200

    almost the final first commit

--- getStats/config_git.php
+++ getStats/config_git.php
@@ -3,8 +3,8 @@
 $config = array(
 
 	"appcode" => "809965",
-	"ubi-email" => "DSCloav@gmail.com",
-	"ubi-password" => "HUFE6587@jceda",
+	"ubi-email" => "",
+	"ubi-password" => "",
 	"default-region" => "emea", //Regions: emea - ncsa - apac 
 	"default-stats" => "casualpvp_death,casualpvp_kdratio,casualpvp_kills", 
 	//All stats: https://gist.github.com/LaxisB/3924cfdc35562b719d1c891cdb895366
